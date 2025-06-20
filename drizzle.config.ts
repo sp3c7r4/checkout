27fb7d2163ee1dd7a3c865f0c04293e316aa1841
Satar Sarafa <sp3c7r40x00@gmail.com>
2025-06-20 21:12:03 +0100

    hi

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit'
+import env from './src/config/env'
+
+export default defineConfig({
+  out: './src/db/migrations',
+  schema: './src/db/schema.ts',
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DB_MEMORY!,
+  },
+})
